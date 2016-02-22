92aacb92dea6e92c65a7c0452e445954eb9413c6
Jesse Mills <jessemills1989@gmail.com>
2016-02-22 09:40:39 -0500

    Comment and clean up profile controller
    This just cleans up (makes prettier) and adds comments to the profile controller

--- controllers/profilesController.js
+++ controllers/profilesController.js
@@ -1,53 +1,69 @@
 var express = require('express');
-var router  = express.Router();
+var router = express.Router();
 var User = require('../models/user.js');
 var request = require('request');
 var marvelbaseURI = 'https://marvel.wikia.com/api/v1/';
 var dcbaseURI = 'https://dc.wikia.com/api/v1/';
 
-
-router.get('/:id',isLoggedIn, function(req,res) {
+// SHOW
+router.get('/:id', isLoggedIn, function(req, res) {
+  // Find the user
   var username = req.params.id;
-  User.findOne({ 'username': username }, function (err, user) {
-    request(marvelbaseURI+'Articles/Details?ids='+user.marvelFavorites+'&abstract=5&width=300&height=300', function(error, response, body) {
+  User.findOne({
+    'username': username
+  }, function(err, user) {
+    // Get the API results for the DC and Marvel favorites so that their favorite cards have information
+    request(marvelbaseURI + 'Articles/Details?ids=' + user.marvelFavorites + '&abstract=5&width=300&height=300', function(error, response, body) {
       if (!error && response.statusCode == 200) {
+        // Store the marvel results
         var marvelBody = JSON.parse(body);
-        request(dcbaseURI+'Articles/Details?ids='+user.dcFavorites+'&abstract=5&width=300&height=300', function(error, response, body) {
+        // Get the DC endpoint
+        request(dcbaseURI + 'Articles/Details?ids=' + user.dcFavorites + '&abstract=5&width=300&height=300', function(error, response, body) {
           if (!error && response.statusCode == 200) {
-            res.render('profile/index.ejs', {user: user, apiResultsmarvel: marvelBody, apiResultsdc: JSON.parse(body)})
-          }
-        })
-      }
-    })
-  })
-})
+            // Send back the data
+            res.render('profile/index.ejs', {
+              user: user,
+              apiResultsmarvel: marvelBody,
+              apiResultsdc: JSON.parse(body)
+            });
+          };
+        });
+      };
+    });
+  });
+});
 
-
+// UPDATE
 router.post('/:id', isLoggedIn, function(req, res) {
+  // Get the user to update and then perform the update
   var username = req.params.id;
-  User.findOneAndUpdate({ 'username': username }, {
-    username: req.body.username,
-    "local.email": req.body.email, 
-    comicSide: req.body.comicSide },
+  User.findOneAndUpdate({
+      'username': username
+    }, {
+      // This doesn't need to include $set as mongoose does this by default
+      username: req.body.username,
+      "local.email": req.body.email,
+      comicSide: req.body.comicSide
+    },
     function(err, user) {
       if (err) {
-        res.redirect('/profile/'+username)
+        // If there's a reason the profile can't be updated, redirect them back to their old profile
+        res.redirect('/profile/' + username)
+      } else {
+        // If it's successful redirect them to the new username in case they changed it
+        res.redirect('/profile/' + req.body.username)
       }
-      else {
-        res.redirect('/profile/'+req.body.username)
-      }
-  })
+    })
 })
 
 
 function isLoggedIn(req, res, next) {
-
-    // if user is authenticated in the session, carry on
-    if (req.isAuthenticated())
-        return next();
-
-    // if they aren't redirect them to the home page
-    res.redirect('/');
+  // if user is authenticated in the session, carry on
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  // if they aren't redirect them to the home page
+  res.redirect('/');
 }
 
 module.exports = router;
