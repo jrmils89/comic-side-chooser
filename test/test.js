05bda972bb202db28c4767a7a0de8ee0fc704ed9
Jesse Mills <jessemills1989@gmail.com>
2016-02-22 12:50:25 -0500

    Small tweaks to tests to add comments

--- test/test.js
+++ test/test.js
@@ -6,39 +6,46 @@ var User = require('../models/user');
 require('../server');
 
 
-describe('GET /marvel', function(){
+describe('GET /marvel: ', function(){
+  // Testing that a request to /marvel returns a success response
   it('GET /marvel', function(done){
     request(url).get('/marvel')
       .expect(200, done);
   })
 });
 
-
-describe('GET /dc', function(){
-  it('GET /marvel', function(done){
+describe('GET /dc: ', function(){
+  // Testing that a request to /marvel returns a success response
+  it('GET /dc', function(done){
     request(url).get('/dc')
       .expect(200, done);
   })
 });
 
-describe('CREATE USER', function() {
+describe('CREATE USER TESTS: ', function() {
+  // Holder var so that a session can be used in other tests
   var Cookies;
+  // Testing the signup functionality
   it('TEST /signup', function(done) {
     request(url)
     .post('/signup')
+    // Dummy user
     .send({"email": "user_test@example.com", "password":"123", "username":"testusername"})
     .set('Accept','application/json')
     .expect(200)
     .end(function(err, res) {
       if (err) throw (err);
+      // Set the cookie to the res cookie so it can be used later
       Cookies = res.headers['set-cookie'];
       done();
     })
   })
 
   it('GET /profile/testusername', function (done) {
+    // Check the profile of the created user loads properly
     request(url).get('/profile/testusername')
       .set('Accept','application/json')
+      // Set the cookie in this request which was provided earlier
       .set('Cookie', Cookies)
       .expect(200)
       .end(function (err, res) {
@@ -47,12 +54,13 @@ describe('CREATE USER', function() {
       });
   });
 
+  // Removes the test user that we created
   it('Remove the test user', function (done) {
+    // Find by the username set above
     User.findOneAndRemove({username: "testusername"}, function(err, data) {
       if (err) throw (err);
       done();
     })
   });
-
 })
 
